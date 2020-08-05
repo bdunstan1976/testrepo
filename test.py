28e6d0dcf934d5dfa01bc95cc876ebe72706a7e0
bdunstan1976 <69271319+bdunstan1976@users.noreply.github.com>
2020-08-05 17:05:51 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+name = input("What's your name?: ")
+
+print("If a small child was trying to get your attention they would say ")
+say = name * 5
+print(say)
+
+
+print("If Shawn was mad at you he would call you",name.upper())
+
